72f08738bc8293adf468f58d51aff3f8fdda5906
Cooper Hartog <75583302+Shark15-55@users.noreply.github.com>
2023-05-18 18:08:45 -0400

    Times Rendering Correctly

---
+++ src/schdule.js
@@ -0,0 +1,56 @@
+import React, { Component } from "react";
+
+function Schdule() {
+  return (
+    <div>
+      <Time />
+    </div>
+  );
+}
+class Time extends Component{
+    constructor(props){
+      super(props);
+      this.days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday',];
+      this.times = ['10:00', '11:00','12:00', '1:00', '2:00', '3:00', '4:00',];
+    }
+    click(i,j){
+      console.log(i,j);
+    }
+    render(){
+      return(
+          <table>
+            <thead>
+              <tr>
+              {this.days.map((day) => (
+                  <th key ={day}>{day}</th>
+              ))
+              }
+              </tr>
+            </thead>
+            <tbody>
+            {
+            this.times.map((time,i) => (
+              <tr key = {time}>
+                {this.days.map((day,j) => (
+                <TimeSlot key = {j*this.days.length + i} onClick = {() => this.click(i,j)} time={time}/>
+                ))
+                }
+              </tr>
+            ))
+            }
+            </tbody>
+          </table>
+      )
+    }
+  }
+  
+  function TimeSlot(props){
+    return(
+        <td>
+          <div onClick = {() => props.onClick()} className = "time">
+            {props.time}
+          </div>
+        </td>
+    )
+  }
+  export default Schdule;
